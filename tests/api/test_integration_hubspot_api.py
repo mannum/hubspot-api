ad7935854c5cec8033315581c1341aa4e99d829f
awornast <alexander.wornast@gosuperscript.com>
2023-03-27 17:49:41 +0100

    Added retry loops for failing tests

--- tests/api/test_integration_hubspot_api.py
+++ tests/api/test_integration_hubspot_api.py
@@ -75,10 +75,9 @@ def test_create_and_find_contact(hubspot_client):
 
     # Added a retry loop because this test is failing
     for i in range(0, 100):
-        print("attempt 1")
         time.sleep(10)
         contact = hubspot_client.find_contact("hs_object_id", contact_result.id)
-        if not contact:
+        if len(contact) > 0:
             break
     assert contact
 
@@ -100,9 +99,13 @@ def test_create_and_find_company(hubspot_client):
     assert company_result.id
 
     # Assert the company now exists based on previous creation
-    time.sleep(7)
-    company = hubspot_client.find_company("hs_object_id", company_result.id)
-    assert company
+
+    for i in range(0, 100):
+        time.sleep(7)
+        company = hubspot_client.find_company("hs_object_id", company_result.id)
+        if len(company) > 0:
+            break
+        assert company
 
 
 def test_create_contact_and_associated_company_with_auto_created_company(
