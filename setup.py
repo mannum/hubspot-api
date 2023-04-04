151383855f2f34ff6bc562fd0e019da359ac9a90
awornast <alexander.wornast@gosuperscript.com>
2023-04-04 16:18:52 +0100

    Updated install method to use setup.cfg and wheel

--- setup.py
+++ setup.py
@@ -1,13 +1,3 @@
 from distutils.core import setup
 
-from setuptools import find_packages
-
-setup(
-    name="hubspot-api",
-    version="1.2.3",
-    description="Superscript Hubspot API",
-    author="Superscript",
-    author_email="paul.lucas@gosuperscript.com",
-    install_requires=["requests", "python-dotenv==0.19.2", "hubspot-api-client==5.0.1"],
-    packages=find_packages(include=["hs_api*"]),
-)
+setup()
